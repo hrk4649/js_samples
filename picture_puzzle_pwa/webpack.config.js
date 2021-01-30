5917479fcb8866e0b75d7925b59baa6688f9be0b
hrk4649 <58840595+hrk4649@users.noreply.github.com>
2021-01-30 21:00:27 +0900

    #2 manifest.json を追加。タイトル画面の画像の縦比率の指定を止める

--- picture_puzzle_pwa/webpack.config.js
+++ picture_puzzle_pwa/webpack.config.js
@@ -1,11 +1,22 @@
 const path = require('path');
+const webpack = require('webpack');
+// Build Feature Flags for Vue
+// https://github.com/vuejs/vue-next/tree/master/packages/vue#bundler-build-feature-flags
+// https://webpack.js.org/plugins/define-plugin/
+
+new webpack.DefinePlugin({
+  '__VUE_OPTIONS_API__': JSON.stringify(true),
+  '__VUE_PROD_DEVTOOLS__': JSON.stringify(false)
+});
+
 
 module.exports = {
   mode: 'development',
+  // mode: 'production',
   devtool: 'source-map',
   devServer: {
       contentBase: path.join(__dirname, 'dist'),
       host: '0.0.0.0',
       port: 3000
-  },
+  }
 };
